4448056d628b8bf262cd4912030429502f9accd4
Jonathan SARDO <sardoj@gmail.com>
2021-03-13 16:15:43 +0100

    feat: Search company data by SIRET

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HomeComponent } from './pages/home/home.component';
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "@nativescript/angular";
+import { NativeScriptHttpClientModule, NativeScriptModule } from "@nativescript/angular";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -11,7 +11,8 @@ import { AppComponent } from "./app.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptHttpClientModule,
     ],
     declarations: [
         AppComponent,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'ns-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: "ns-home",
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+    // Documentation API: https://entreprise.data.gouv.fr/api_doc/sirene
 
-  constructor() { }
+    public company:any;
 
-  ngOnInit() {
-  }
+    constructor(private http: HttpClient) {}
 
+    ngOnInit() {}
+
+    searchCompany() {
+        let url: string = "https://entreprise.data.gouv.fr/api/sirene/v3/etablissements/";
+        let siret: string = "52282368100047";
+
+        this.http.get(url + siret).subscribe((response) => {
+            this.company = response;
+        });
+    }
 }
